77fa2852310e241393077b10395588a3a80dc310
thuc95 <thuc201995@gmail.com>
2020-08-23 18:40:13 +0700

    change structure + test rebase

--- src/components/List.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const List = () => {
-  return;
-};
-
-export default List;
--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,7 +1,15 @@
 import React, { memo } from "react";
 import { Layout } from "../components";
+import CustomerPopover from "../features/customerPopover";
 const Dashboard = () => {
-  return <Layout></Layout>;
+  return (
+    <Layout style={{ height: "200vh" }}>
+      <Layout.Header title="DashBoard"></Layout.Header>
+      <Layout.Body>
+        <CustomerPopover></CustomerPopover>
+      </Layout.Body>
+    </Layout>
+  );
 };
 
 export default memo(Dashboard);
